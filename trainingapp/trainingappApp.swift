c4b0fe44572596d0ad6d33f202a42006a7de708a
Yusuke KUWAHARA <barabaaaara0127@gmail.com>
2021-06-21 21:42:33 +0900

    Initial Commit

---
+++ trainingapp/trainingappApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingappApp.swift
+//  trainingapp
+//
+//  Created by 桑原佑輔 on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct trainingappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
